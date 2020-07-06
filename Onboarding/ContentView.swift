bcfd3eb033f81671fe9e3d3cc7ff942415a86f03
Augustinas Malinauskas <augustinasmal@gmail.com>
2020-07-06 08:40:06 +0100

    Initial Commit

---
+++ Onboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onboarding
+//
+//  Created by Augustinas Malinauskas on 06/07/2020.
+//  Copyright © 2020 Augustinas Malinauskas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
